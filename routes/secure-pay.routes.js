a68cbabb4f68a6e7e821058f6f0acc03c95c663b
addymwenda12 <muneneaddy@gmail.com>
2024-08-29 12:21:29 +0300

    Added route for fraud detection

--- routes/secure-pay.routes.js
+++ routes/secure-pay.routes.js
@@ -1,7 +1,8 @@
 const express = require('express')
-
+const fraudDetectorController = require('../controllers/fraud-detector.controller');
 
 const secureRouter = express.Router()
 
+secureRouter.post('/fraud-detector', fraudDetectorController.fraudDetector);
 
 module.exports = secureRouter
